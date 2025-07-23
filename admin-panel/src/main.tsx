7693c6ad1f3d73a2fe4546796f5fa2d1d96de4f2
Hashir V <hashirventhodi@gmail.com>
2025-07-23 15:51:17 +0530

    basepath update ofr the production in tanstack router

--- admin-panel/src/main.tsx
+++ admin-panel/src/main.tsx
@@ -73,6 +73,7 @@ const router = createRouter({
   context: { queryClient },
   defaultPreload: 'intent',
   defaultPreloadStaleTime: 0,
+  basepath: import.meta.env.PROD ? '/admin' : '/',
 })
 
 // Register the router instance for type safety
