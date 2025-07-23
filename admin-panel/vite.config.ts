ab5399f30937a711c50f467bc9053ea6b68c599c
Hashir V <hashirventhodi@gmail.com>
2025-07-23 14:56:04 +0530

    update the base path for production

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/admin/' : '/',
   plugins: [
     TanStackRouterVite({
       target: 'react',
